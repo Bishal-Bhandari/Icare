71fae296f5324b973f3d4994441a8818d3cd5fd4
Bishal Bhandari <bishalbhaandari62@gmail.com>
2020-02-03 09:17:11 +0545

    DB maintained

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","admin","factory_ra");
+$con=mysqli_connect("localhost","root","admin","icare_db");
 if(mysqli_connect_error())
 {
 	echo "Failed to connect".mysqli_connect_error();
