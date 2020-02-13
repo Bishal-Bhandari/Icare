a9ad41eb4ea4b6fd2a394cbdfdfabed82fa63b76
Bishal Bhandari <bishalbhaandari62@gmail.com>
2020-02-13 11:03:38 +0545

    change

--- index.php
+++ index.php
@@ -7,146 +7,156 @@ include('include/navbar.php');
 include('include/slider.php');
 ?>
 
-  <!--===== welcome speech =====-->
-  <section class="container-fluid pb-3 mt-5 py-0">
-        <div class="row px-lg-5 px-2 mx-0">
-          <div class="col-lg-8 col-sm-12 pl-lg-5 px-2" data-aos="fade-right" data-aos-duration="1000">
-            <h3 class="display-5 pt-5 font-weight-bold service-heading-top head-heading-top">Welcome to ICare Technology</h3>
+<!--===== welcome speech =====-->
+<section class="container-fluid pb-3 mt-5 py-0">
+    <div class="row px-lg-5 px-2 mx-0">
+        <div class="col-lg-8 col-sm-12 pl-lg-5 px-2" data-aos="fade-right" data-aos-duration="1000">
+            <h3 class="display-5 pt-5 font-weight-bold service-heading-top head-heading-top">Welcome to ICare Technology
+            </h3>
             <hr class="underline-welcome-about ml-0 mt-0 ">
-            <p class="para-service line-height-about mt-4 mb-1 pb-3 text-dark">Welcome you to the ICare Technology Pvt. Ltd.
-			 Since 2011, ICare has been a leading provider of ICT solutions and services. It is ultimate place for ICT solution. 
-			We are known for providing the best IT experience and taking great care of our clients and employees.</p>
-            <p class="para-service line-height-about mb-3 pb-5 text-dark">We ICare Technology have been working on the Web Application,
-			 Mobile Application. The quality and reliability of our products have always satisfied our clients. The develpment of the wbsite 
-			 and applications is passion to us so we create your dream while enjoying. Contact us for more your dream project.</p>
+            <p class="para-service line-height-about mt-4 mb-1 pb-3 text-dark">Welcome you to the ICare Technology Pvt.
+                Ltd.
+                Since 2011, ICare has been a leading provider of ICT solutions and services. It is ultimate place for
+                ICT solution.
+                We are known for providing the best IT experience and taking great care of our clients and employees.
+            </p>
+            <p class="para-service line-height-about mb-3 pb-5 text-dark">We ICare Technology have been working on the
+                Web Application,
+                Mobile Application. The quality and reliability of our products have always satisfied our clients. The
+                develpment of the wbsite
+                and applications is passion to us so we create your dream while enjoying. Contact us for more your dream
+                project.</p>
 
-          </div>
+        </div>
 
-          <div class="col-lg-4 col-sm-12 pr-lg-5 px-2" data-aos="fade-left" data-aos-duration="1000">
+        <div class="col-lg-4 col-sm-12 pr-lg-5 px-2" data-aos="fade-left" data-aos-duration="1000">
             <img src="img/team/ceo1.jpg" width="100%" alt="Image">
-          </div>
+        </div>
 
-        </div>
-  </section>
-
-  <!-- Services -->
-  <section class="page-section" id="services">
-    <div class="container">
-      <div class="row mx-0">
-        <div class="col-lg-12 text-center">
-          <h2 class="section-heading text-uppercase head-heading-top">Feature</h2>
-          <h3 class="section-subheading text-muted">Here we have feature of our company.</h3>
-        </div>
-      </div>
-      <div class="row text-center mx-0">
-        <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
-          <span class="fa-stack fa-4x">
-			<i class="fas fa-circle fa-stack-2x text-primary"></i>
-			<i class="fas fa-pencil-ruler fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">Design</h4>
-          <p class="text-muted">We collect requirements from our client and study/research on it. Then our UI/UX team start on the design for the system.</p>
-        </div>
-        <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
-          <span class="fa-stack fa-4x">
-			<i class="fas fa-circle fa-stack-2x text-primary"></i>
-			<i class="fas fa-file-code fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">Develop</h4>
-          <p class="text-muted">Our coders will change the design into running system. The optimized system that meets every requirements will be deliverd.</p>
-        </div>
-        <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
-          <span class="fa-stack fa-4x">
-			<i class="fas fa-circle fa-stack-2x text-primary"></i>
-			<i class="fas fa-server fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 class="service-heading">Deploy</h4>
-          <p class="text-muted">We go through various testing with the system. After the satisfaction of our clients we deploy the system onto the servers.</p>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
 
-	<!--===== general =====-->
-	<section class="container-fluid pl-lg-1 pr-lg-4 my-5 py-5" id="general">
-			<div class="row pl-2 mx-0">
-				<div class="col-lg-7">
-					<img src="img/info-bg.jpeg" width="100%">
-				</div>
-				<div class="col-lg-5 pl-3">
-					<div class="row mx-0 bg-light py-2 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
-						<div class="col-sm-12">
-							<article class="px-1">
-								<i class="fab fa-500px fa-2x mb-3"></i>
-								<!-- <h5 class="font-weight-bold my-2">Events</h5> -->
-								<p class="para-color">We use latest methods for designing clients project(s).
-								</p>
-							</article>
-						</div>
-					</div>
-					<div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
-						<div class="col-sm-12">
-							<article class="px-1">
-								<i class="fas fa-calendar-week fa-2x mb-3"></i>
-								<!-- <h5 class="font-weight-bold my-2">Schedule</h5> -->
-								<p class="para-color">We guarantee to complete the project(s) within the dateline.
-								</p>
-							</article>
-						</div>
-					</div>
-					<div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
-						<div class="col-sm-12">
-							<article class="px-1">
-								<i class="fas fa-medal fa-2x mb-3"></i>
-								<!-- <h5 class="font-weight-bold my-2">Reward</h5> -->
-								<p class="para-color">We design and develop best of the best system among other.
-								</p>
-							</article>
-						</div>
-					</div>
-					<div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
-						<div class="col-sm-12">
-							<article class="px-1">
-								<i class="far fa-heart fa-2x mb-3"></i>
-								<!-- <h5 class="font-weight-bold my-2">Donation</h5> -->
-								<p class="para-color">Our support towards the client will forever and always.
-								</p>
-							</article>
-						</div>
-					</div>
-				</div>
-			</div>
-	</section>
+<!-- Services -->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="row mx-0">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading text-uppercase head-heading-top">Feature</h2>
+                <h3 class="section-subheading text-muted">Here we have feature of our company.</h3>
+            </div>
+        </div>
+        <div class="row text-center mx-0">
+            <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-pencil-ruler fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="service-heading">Design</h4>
+                <p class="text-muted">We collect requirements from our client and study/research on it. Then our UI/UX
+                    team start on the design for the system.</p>
+            </div>
+            <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-file-code fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="service-heading">Develop</h4>
+                <p class="text-muted">Our coders will change the design into running system. The optimized system that
+                    meets every requirements will be deliverd.</p>
+            </div>
+            <div class="col-md-4" data-aos="flip-up" data-aos-duration="1000">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-server fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="service-heading">Deploy</h4>
+                <p class="text-muted">We go through various testing with the system. After the satisfaction of our
+                    clients we deploy the system onto the servers.</p>
+            </div>
+        </div>
+    </div>
+</section>
 
-  
-	<!--===== number of project info section =====-->
-  <section id="numberProject" class="container-fluid px-lg-0 py-5 d-none d-lg-block">
-			<div class="row counter-row-img mx-0">
-				<div class="text-center show-project-num">
-				<span class="iconify counter-icon" data-icon="entypo:emoji-happy" data-inline="false"></span>
-					<p class="value c-section4 mb-5">56</p>
-					<p class="text-uppercase counter-name">Happy Clients</p>
-				</div>
-				<div class="bar"></div>
-				<div class="text-center show-project-num ">
-				<span class="iconify counter-icon" data-icon="whh:website" data-inline="false"></span>
-					<p class="value c-section4 mb-5">23</p>
-					<p class="text-uppercase counter-name">Website</p>
-				</div>
-				<div class="bar"></div>
-				<div class="text-center show-project-num ">
-				<span class="iconify counter-icon" data-icon="whh:seo" data-inline="false"></span>
-					<p class="value c-section4 mb-5">35</p>
-					<p class="text-uppercase counter-name">SEO</p>
-				</div>
-				<div class="bar"></div>
-				<div class="text-center show-project-num">
-				<span class="iconify counter-icon" data-icon="ic:baseline-touch-app" data-inline="false"></span>
-					<p class="value c-section4 mb-5">13</p>
-					<p class="text-uppercase counter-name">Application</p>
-				</div>
-			</div>
-  </section>
+<!--===== general =====-->
+<section class="container-fluid pl-lg-1 pr-lg-4 my-5 py-5" id="general">
+    <div class="row pl-2 mx-0">
+        <div class="col-lg-7">
+            <img src="img/info-bg.jpeg" width="100%">
+        </div>
+        <div class="col-lg-5 pl-3">
+            <div class="row mx-0 bg-light py-2 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
+                <div class="col-sm-12">
+                    <article class="px-1">
+                        <i class="fab fa-500px fa-2x mb-3"></i>
+                        <!-- <h5 class="font-weight-bold my-2">Events</h5> -->
+                        <p class="para-color">We use latest methods for designing clients project(s).
+                        </p>
+                    </article>
+                </div>
+            </div>
+            <div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
+                <div class="col-sm-12">
+                    <article class="px-1">
+                        <i class="fas fa-calendar-week fa-2x mb-3"></i>
+                        <!-- <h5 class="font-weight-bold my-2">Schedule</h5> -->
+                        <p class="para-color">We guarantee to complete the project(s) within the dateline.
+                        </p>
+                    </article>
+                </div>
+            </div>
+            <div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
+                <div class="col-sm-12">
+                    <article class="px-1">
+                        <i class="fas fa-medal fa-2x mb-3"></i>
+                        <!-- <h5 class="font-weight-bold my-2">Reward</h5> -->
+                        <p class="para-color">We design and develop best of the best system among other.
+                        </p>
+                    </article>
+                </div>
+            </div>
+            <div class="row mx-0 bg-light py-2 my-3 general-content-shadow" data-aos="flip-up" data-aos-duration="1000">
+                <div class="col-sm-12">
+                    <article class="px-1">
+                        <i class="far fa-heart fa-2x mb-3"></i>
+                        <!-- <h5 class="font-weight-bold my-2">Donation</h5> -->
+                        <p class="para-color">Our support towards the client will forever and always.
+                        </p>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!--===== number of project info section =====-->
+<section id="numberProject" class="container-fluid px-lg-0 py-5 d-none d-lg-block">
+    <div class="row counter-row-img mx-0">
+        <div class="text-center show-project-num">
+            <span class="iconify counter-icon" data-icon="entypo:emoji-happy" data-inline="false"></span>
+            <p class="value c-section4 mb-5">56</p>
+            <p class="text-uppercase counter-name">Happy Clients</p>
+        </div>
+        <div class="bar"></div>
+        <div class="text-center show-project-num ">
+            <span class="iconify counter-icon" data-icon="whh:website" data-inline="false"></span>
+            <p class="value c-section4 mb-5">23</p>
+            <p class="text-uppercase counter-name">Website</p>
+        </div>
+        <div class="bar"></div>
+        <div class="text-center show-project-num ">
+            <span class="iconify counter-icon" data-icon="whh:seo" data-inline="false"></span>
+            <p class="value c-section4 mb-5">35</p>
+            <p class="text-uppercase counter-name">SEO</p>
+        </div>
+        <div class="bar"></div>
+        <div class="text-center show-project-num">
+            <span class="iconify counter-icon" data-icon="ic:baseline-touch-app" data-inline="false"></span>
+            <p class="value c-section4 mb-5">13</p>
+            <p class="text-uppercase counter-name">Application</p>
+        </div>
+    </div>
+</section>
 
 
 <?php
@@ -198,23 +208,25 @@ include('include/slider.php');
 ?>
 
 
-  <!--===== first section ======-->
-  <section class="container-fluid px-lg-5 py-1 my-5" id="#">
-			<div class="row px-lg-5 px-2 mt-lg-5 pt-lg-5 py-3 mx-0">
-				<div class="col-lg-5 pt-0 pr-lg-5">
-					<h4 class="font-weight-bold heading-font-size head-heading-top">Some overview on our organization</h4>
-					<hr class="underline-bar-indexSecondary">
-					<p class="para-firstSection-indexSecondary pt-lg-4 mt-1">I am satisfied with the work and performance of the ICare team. They have done great job for my website. </p>
-					<p class="para-firstSection-indexSecondary">They have completed the website within the dateline and within the feasibility of the cost. The website is great as per my request. Once again I want to thank ICare and its team for its brillent job.</p>
-				</div>
-				<div class="col-lg-7 py-2 pr-lg-5 px-3">
-					<img src="img/person.jpeg" width="100%">
-				</div>
-			</div>
-	</section>
+<!--===== first section ======-->
+<section class="container-fluid px-lg-5 py-1 my-5" id="#">
+    <div class="row px-lg-5 px-2 mt-lg-5 pt-lg-5 py-3 mx-0">
+        <div class="col-lg-5 pt-0 pr-lg-5">
+            <h4 class="font-weight-bold heading-font-size head-heading-top">Some overview on our organization</h4>
+            <hr class="underline-bar-indexSecondary">
+            <p class="para-firstSection-indexSecondary pt-lg-4 mt-1">I am satisfied with the work and performance of the
+                ICare team. They have done great job for my website. </p>
+            <p class="para-firstSection-indexSecondary">They have completed the website within the dateline and within
+                the feasibility of the cost. The website is great as per my request. Once again I want to thank ICare
+                and its team for its brillent job.</p>
+        </div>
+        <div class="col-lg-7 py-2 pr-lg-5 px-3">
+            <img src="img/person.jpeg" width="100%">
+        </div>
+    </div>
+</section>
 
-  <!-- footer -->
+<!-- footer -->
 <?php 
 include('include/footer.php');
  ?>
- 
